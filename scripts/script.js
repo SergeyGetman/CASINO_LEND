df776e81dce9af5b9b3a5da5f4d1b77ba39ba714
Sergii Getman <sergeygetman2020@gmail.com>
2024-08-16 02:47:40 +0300

    add logick show modal

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,10 @@ function showModal() {
 		`
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
+
+          
+              <img src="./assets/images/bonuses/bonus_plus_100.png" alt="bonus">
+ 
         </div>
       </div>
           `
@@ -30,21 +21,37 @@ function showModal2() {
 	document.body.insertAdjacentHTML(
 		'beforeend',
 		`
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="winner-modal-100" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
           
-                 <img src="./assets/images/bonuses/bonus-general.png" alt="bonus">
+                 <img id="rotatingImage" src="./assets/images/bonuses/bonus-general.png" alt="bonus">
           </div>
         </div>
       </div>
           `
 	);
 
-	const modal = new bootstrap.Modal(document.getElementById('exampleModal'));
+	const modal = new bootstrap.Modal(document.getElementById('winner-modal-100'));
 	return modal.show();
 }
 
 
+
+
+function checkedValueSpin(spinCount) {
+
+  switch (spinCount) {
+      case 1:
+          showModal2();
+          break;
+      case 2:
+          showModal();
+          break;
+      default:
+          alert("This is impossible");
+          break;
+  }
+}
 
 	let counter = 0;
 
@@ -52,11 +59,16 @@ function showModal2() {
 		const img = document.getElementById('rotatingImage');
 
 		counter += 1;
+    
 
-	 return	counter === 1
+	 	counter === 1
 			? (img.style.animation = `rotateForHundred 3s ease-in-out forwards`)
 			: (img.style.animation = `rotate 3s ease ${1}`);
 
+      setTimeout(() => {
+         checkedValueSpin(counter)
+      }, 3000)
+     
 	}
 
 
